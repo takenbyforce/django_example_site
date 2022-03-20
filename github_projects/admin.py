92bfce70808849a948ea2b9e57da0952b5b61b0d
Roman Sinitsyn <ryusinitsyn@gmail.com>
2022-03-20 16:46:56 +0200

    Add `ProjectEntry` model

--- github_projects/admin.py
+++ github_projects/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProjectEntry
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('owner',)
+    list_display = ('name', 'rating', 'owner')
+    list_filter = ('rating', 'owner')
+
+    def get_changeform_initial_data(self, request):
+        """Use current user as a default for `owner` field"""
+        initial = super().get_changeform_initial_data(request)
+        return {**initial, 'owner': request.user}
+
+
+admin.site.register(ProjectEntry, ProjectAdmin)
--- github_projects/models.py
+++ github_projects/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Rating(models.IntegerChoices):
+    ONE = 1, _('1')
+    TWO = 2, _('2')
+    THREE = 3, _('3')
+    FOUR = 4, _('4')
+    FIVE = 5, _('5')
+
+
+class ProjectEntry(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    link = models.URLField()
+    rating = models.PositiveSmallIntegerField(choices=Rating.choices, blank=False, default=Rating.FIVE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{super().__str__()}: {self.name}'
