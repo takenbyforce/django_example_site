ebc4fc6f50405d8d0c16c677dbc3a08b9e965f1a
Roman Sinitsyn <ryusinitsyn@gmail.com>
2022-03-20 20:15:45 +0200

    Display owner's username in `ProjectEntry` API

--- github_projects/serializers.py
+++ github_projects/serializers.py
@@ -5,6 +5,7 @@ from .models import ProjectEntry
 
 class ProjectEntrySerializer(serializers.ModelSerializer):
     owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    username = serializers.CharField(source='owner.username', read_only=True)
 
     class Meta:
         model = ProjectEntry
